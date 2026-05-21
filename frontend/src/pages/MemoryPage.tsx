b9bbab0aeb020e17514df88bef33c08e2acac162
Greg Iteen <gregiteen@gmail.com>
2026-05-21 16:43:29 -0600

    fix(frontend): disable react-hooks/set-state-in-effect warning for synchronous setSelected

--- frontend/src/pages/MemoryPage.tsx
+++ frontend/src/pages/MemoryPage.tsx
@@ -60,6 +60,7 @@ export default function MemoryPage() {
       }
       loadUrlSlug()
     } else {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setSelected(null)
     }
   }, [urlSlug])
